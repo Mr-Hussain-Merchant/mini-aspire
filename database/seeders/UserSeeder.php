09abccd0bb26d70b3a0a2e700b300e34fc30777d
Mr-Hussain-Merchant <m.hussain.merchant@gmail.com>
2022-10-31 23:30:12 +0530

    customer seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -23,5 +23,14 @@ class UserSeeder extends Seeder
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s')
         ]);
+
+        DB::table('users')->insert([
+            'name' => "Hussain Merchant",
+            'email' => "hussain@gmail.com",
+            'password' => Hash::make('123456'),
+            'remember_token' => Str::random(60),
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
     }
 }
